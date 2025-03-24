e4a2a1a23ae3cd5d6b31263e67cc924ee9d6c169
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-03-25 00:14:37 +0200

    Add Not Found page with image and styling; implement routing to home

--- src/app/Pages/notfound/notfound.component.ts
+++ src/app/Pages/notfound/notfound.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RoutingModule } from '../../core/Shared/Module/routing/routing.module';
 
 @Component({
   selector: 'app-notfound',
-  imports: [],
+  imports: [RoutingModule],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.scss'
 })
